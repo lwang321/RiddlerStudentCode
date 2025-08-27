3ae8351c6282caa5937c20edd13617286bd3b460
Lucas Wang <lucas.wang@menloschool.org>
2025-08-27 15:23:09 -0700

    Test commit

--- src/Riddler.java
+++ src/Riddler.java
@@ -6,12 +6,28 @@
  *
  * Completed by: YOUR NAME HERE
  */
+
+
 public class Riddler {
 
     public String decryptOne(String encrypted) {
         String decrypted = "";
 
         // TODO: Complete the decryptOne() function.
+        for (int i=0; i<encrypted.length(); i++)
+        {
+            char c = encrypted.charAt(i);
+            char p;
+            if (Character.isUpperCase(c))
+            {
+                p = (char) (((int) c + 9 - 'A') % 26 + 'A');
+            }
+            else
+            {
+                p = (char) (((int) c + 9 - 'a') % 26 + 'a');
+            }
+            decrypted = decrypted + String.valueOf(p);
+        }
 
         return decrypted;
     }
@@ -19,6 +35,13 @@ public class Riddler {
     public String decryptTwo(String encrypted) {
         String decrypted = "";
 
+        String[] nums = encrypted.split(" ");
+
+        for (int i=0; i<nums.length; i++)
+        {
+            char c = (char) ((int) nums[i]);
+            decrypted = decrypted + String.valueOf(c);
+        }
         // TODO: Complete the decryptTwo() function.
 
         return decrypted;
@@ -26,6 +49,14 @@ public class Riddler {
 
     public String decryptThree(String encrypted) {
         String decrypted = "";
+
+
+        for (int i=0; i<encrypted.length/8; i++)
+        {
+            String num = encrypted.substring(i * 8, i * 8 + 8);
+            char c = (char) Integer.parseInt(num, 2);
+            decrypted = decrypted + String.valueOf(c);
+        }
 
         // TODO: Complete the decryptThree() function.
 
@@ -35,6 +66,11 @@ public class Riddler {
     public String decryptFour(String encrypted) {
         String decrypted = "";
 
+        for (int i=0; i<encrypted.length; i++)
+        {
+            char c = (char)((int) encrypted.charAt(i) - 9919);
+            decrypted = decrypted + String.valueOf(c);
+        }
         // TODO: Complete the decryptFour() function.
 
         return decrypted;
